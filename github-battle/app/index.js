69c74f88cb2805c12a561f5232735b52a43934c4
Mohanad Abu-Nayla <aljaboori@gmail.com>
2018-10-01 23:22:35 +0800

    Popular comp and styling

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,10 +1,23 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-require("index.css");
-class App extends React.Component {
+var PropTypes = require("prop-types");
+require("./index.css");
+class Badge extends React.Component {
   render() {
-    return <div>Hello React</div>;
+    return;
+    <div>
+      <img
+        src={this.props.img}
+        alt="Avatar"
+        style={{ width: 100, height: 100 }}
+      />
+      <h1>name = {this.props.name}</h1>
+      <h3>this.props.username</h3>
+    </div>;
   }
 }
 
-ReactDOM.render(<app />, document.getElementById("app"));
+ReactDOM.render(
+  <Badge name="Tyler" username="tyler" img="" />,
+  document.getElementById("app")
+);
